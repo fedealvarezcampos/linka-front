70bf667991feff486b10b08dda0439a1ca7f1730
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-07 13:05:49 +0100

    SortMenu timer

--- src/components/NavSort.js
+++ src/components/NavSort.js
@@ -1,76 +1,84 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import '../styles/NavSort.css';
 
 function NavSort({ sort, setSort, page, setPage }) {
-    const user = useSelector(s => s.user);
+	const user = useSelector(s => s.user);
 
-    const [sortMenu, setSortMenu] = useState(false);
+	const [sortMenu, setSortMenu] = useState(false);
 
-    const handleSorting = sorting => {
-        setSortMenu(false);
-        setSort(sorting);
-        page && setPage(2);
-        window.scrollTo(0, 0);
-    };
+	useEffect(() => {
+		const timeoutID = setTimeout(() => {
+			sortMenu && setSortMenu(false);
+		}, 3000);
 
-    const mostRecent = (
-        <>
-            <i className="bi bi-alarm-fill" />
-            <span>Most recent</span>
-        </>
-    );
+		return () => clearTimeout(timeoutID);
+	}, [sortMenu]);
 
-    const bestToday = (
-        <>
-            <i className="bi bi-trophy-fill" />
-            <span>Top rated</span>
-        </>
-    );
+	const handleSorting = sorting => {
+		setSortMenu(false);
+		setSort(sorting);
+		page && setPage(2);
+		window.scrollTo(0, 0);
+	};
 
-    const mostDiscussed = (
-        <>
-            <i className="bi bi-chat-square-dots-fill" />
-            <span>Most discussed</span>
-        </>
-    );
+	const mostRecent = (
+		<>
+			<i className="bi bi-alarm-fill" />
+			<span>Most recent</span>
+		</>
+	);
 
-    return (
-        <>
-            {user && !sortMenu && (
-                <div className="sortMenuReleaseContainer">
-                    <div className="sortMenuRelease" onMouseOver={() => setSortMenu(true)}>
-                        {(sort === '' && mostRecent) ||
-                            (sort === 'mostliked' && bestToday) ||
-                            (sort === 'discussed' && mostDiscussed)}
-                    </div>
-                </div>
-            )}
-            {user && sortMenu && (
-                <div className="sortMenuContainer">
-                    <div
-                        className="sortMenu dropMenu"
-                        onMouseOver={() => setSortMenu(true)}
-                        onMouseLeave={() => setSortMenu(false)}
-                    >
-                        <div className="sortMenuLinkTitle">
-                            <i className="bi bi-filter-square-fill" />
-                            <span>Sorting by:</span>
-                        </div>
-                        <div className="sortMenuLink">
-                            <div onClick={() => handleSorting('')}>{mostRecent}</div>
-                        </div>
-                        <div className="sortMenuLink">
-                            <div onClick={() => handleSorting('mostliked')}>{bestToday}</div>
-                        </div>
-                        <div className="sortMenuLink" onClick={() => handleSorting('discussed')}>
-                            {mostDiscussed}
-                        </div>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+	const bestToday = (
+		<>
+			<i className="bi bi-trophy-fill" />
+			<span>Top rated</span>
+		</>
+	);
+
+	const mostDiscussed = (
+		<>
+			<i className="bi bi-chat-square-dots-fill" />
+			<span>Most discussed</span>
+		</>
+	);
+
+	return (
+		<>
+			{user && !sortMenu && (
+				<div className="sortMenuReleaseContainer">
+					<div className="sortMenuRelease" onMouseOver={() => setSortMenu(true)}>
+						{(sort === '' && mostRecent) ||
+							(sort === 'mostliked' && bestToday) ||
+							(sort === 'discussed' && mostDiscussed)}
+					</div>
+				</div>
+			)}
+			{user && sortMenu && (
+				<div className="sortMenuContainer">
+					<div
+						className="sortMenu dropMenu"
+						onMouseEnter={() => setSortMenu(true)}
+						onMouseLeave={() => setSortMenu(false)}
+					>
+						<div className="sortMenuLinkTitle">
+							<i className="bi bi-filter-square-fill" />
+							<span>Sorting by:</span>
+						</div>
+						<div className="sortMenuLink">
+							<div onClick={() => handleSorting('')}>{mostRecent}</div>
+						</div>
+						<div className="sortMenuLink">
+							<div onClick={() => handleSorting('mostliked')}>{bestToday}</div>
+						</div>
+						<div className="sortMenuLink" onClick={() => handleSorting('discussed')}>
+							{mostDiscussed}
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default NavSort;
